505aba394d68babca3189d46166a6c42719f19d9
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-04-14 20:03:33 +0200

    Use fontawesome's brands icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,99 +1,106 @@
-import React from "react"
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
+import React from "react";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faGithub,
+    faTwitter,
+    faDiscourse,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Header = ({ siteTitle, menuLinks }) => (
     <footer className="bg-primary text-center text-white">
         <div className="container p-4">
             <section className="mb-4">
                 <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                ><i className="fab fa-twitter"></i
-                ></Link>
+                ><FontAwesomeIcon icon={faTwitter}/></Link>
 
                 <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                ><i className="fab fa-github"></i
-                ></Link>
+                ><FontAwesomeIcon icon={faGithub}/></Link>
+
+                <Link className="btn btn-outline-light btn-floating m-1" to="#!" role="button"
+                ><FontAwesomeIcon icon={faDiscourse}/></Link>
             </section>
 
-            <section className="">
-                <div className="row">
-                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5>Section</h5>
+            {/*<section className="">*/}
+            {/*    <div className="row">*/}
+            {/*        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">*/}
+            {/*            <h5>Section</h5>*/}
 
-                        <ul className="list-unstyled mb-0">
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                        </ul>
-                    </div>
+            {/*            <ul className="list-unstyled mb-0">*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*            </ul>*/}
+            {/*        </div>*/}
 
-                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5>Section</h5>
+            {/*        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">*/}
+            {/*            <h5>Section</h5>*/}
 
-                        <ul className="list-unstyled mb-0">
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                        </ul>
-                    </div>
+            {/*            <ul className="list-unstyled mb-0">*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*            </ul>*/}
+            {/*        </div>*/}
 
-                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5>Section</h5>
+            {/*        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">*/}
+            {/*            <h5>Section</h5>*/}
 
-                        <ul className="list-unstyled mb-0">
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                        </ul>
-                    </div>
+            {/*            <ul className="list-unstyled mb-0">*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*            </ul>*/}
+            {/*        </div>*/}
 
-                    <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                        <h5>Section</h5>
+            {/*        <div className="col-lg-3 col-md-6 mb-4 mb-md-0">*/}
+            {/*            <h5>Section</h5>*/}
 
-                        <ul className="list-unstyled mb-0">
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                            <li>
-                                <Link to="#!" className="text-white">Link 1</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
+            {/*            <ul className="list-unstyled mb-0">*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*                <li>*/}
+            {/*                    <Link to="#!" className="text-white">Link 1</Link>*/}
+            {/*                </li>*/}
+            {/*            </ul>*/}
+            {/*        </div>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
         </div>
 
         <hr/>
