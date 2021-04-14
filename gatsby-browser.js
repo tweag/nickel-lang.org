8fbe79d285f76396dbf04bf610eb426f07f31d2f
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-04-14 13:18:18 +0200

    Install gatsby image plugin

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.min.js";
 import "popper.js/dist/popper.min";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "./src/styles/global.css";
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,25 @@
 module.exports = {
-  siteMetadata: {
-    title: "nickel-lang.org",
-    menuLinks: [
-      {
-        name: 'Getting started',
-        link: '#'
-      },
-      {
-        name: 'Documentation',
-        link: '#'
-      },
-      {
-        name: 'Playground',
-        link: '#'
-      },
-    ]
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    siteMetadata: {
+        title: "nickel-lang.org",
+        menuLinks: [
+            {
+                name: 'Getting started',
+                link: '#'
+            },
+            {
+                name: 'Documentation',
+                link: '#'
+            },
+            {
+                name: 'Playground',
+                link: '#'
+            },
+        ]
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-image',
+    ],
 };
