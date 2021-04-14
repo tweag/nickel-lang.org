2794f0bfef5586a52dc747bbb63bfd373f4bc03f
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-04-14 20:03:51 +0200

    Improve landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Layout from "../components/layout"
 import {StaticImage} from "gatsby-plugin-image";
 import '../asciinema-player/asciinema-player.css';
 
+import atomSVG from '../images/atom-electrons.svg';
+import moleculeSVG from '../images/molecules.svg';
+import atomSquareSVG from '../images/atom-square.svg';
+
 // markup
 const IndexPage = () => {
   return (
@@ -10,8 +14,7 @@ const IndexPage = () => {
         <main className="container main-container">
           <section className="row section-block">
             <div className="col text-center">
-              <h1 className="main-title mb-4"><StaticImage className="logo" src="" alt="logo"/> <span
-                  className="align-bottom">Nickel</span></h1>
+              <h1 className="main-title mb-4"><StaticImage className="logo" src="../images/icon.png" alt="logo"/><span className="align-top">Nickel</span></h1>
               <div className="main-subtitle mb-4">Better configuration
                 for less
               </div>
@@ -29,8 +32,8 @@ const IndexPage = () => {
 
           <section className="row section-block">
             <div className="col-4 main-text text-center landingpage-column">
-              <StaticImage src="../images/filler.svg.png" className="abstract-illustration" alt={""}/>
-                <h3>Merge</h3>
+              <img src={atomSVG} className="abstract-illustration" alt={""}/>
+                <h3 className="mb-4 mt-4">Merge</h3>
                 <div className="text-left">
                     Write your configuration as logical blocks in a modular
                       way.
@@ -39,18 +42,22 @@ const IndexPage = () => {
                 </div>
             </div>
             <div className="col-4 main-text text-center landingpage-column">
-              <StaticImage src="../images/filler.svg.png" className="abstract-illustration" alt={""}/>
-                <h3>Verify & Validate</h3>
+              <img src={atomSquareSVG} className="abstract-illustration" alt={""}/>
+                <h3 className="mb-4 mt-4">Verify & Validate</h3>
+              <div className="text-left">
                 Use types to specify and verify functions, if you want to. Let
                 the type inference engine do the boring work. Use contracts to
                 encode arbitrary schemas and validate your configuration data.
+              </div>
             </div>
             <div className="col-4 main-text text-center landingpage-column">
-              <StaticImage src="../images/filler.svg.png" className="abstract-illustration" alt={""}/>
-                <h3>Reuse</h3>
+              <img src={moleculeSVG} className="abstract-illustration" alt={""}/>
+                <h3 className="mb-4 mt-4">Reuse</h3>
+              <div className="text-left">
                 Don't hack, don't reinvent the wheel: Nickel is a proper
                 programming language. Factorize and reuse the generic aspects of
                 configuration, or use existing librairies.
+              </div>
             </div>
           </section>
         </main>
