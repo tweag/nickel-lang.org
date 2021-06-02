36560f7589eb5676d04e709fe2f2f3a11cb4f734
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-06-02 19:34:47 +0200

    [WIP]Replace asciinema with a playground in the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,54 @@
 import * as React from "react"
 import Layout from "../components/layout"
 import {StaticImage} from "gatsby-plugin-image";
-
-import atomSVG from '../images/atom-electrons.svg';
 import moleculeSVG from '../images/molecules.svg';
 import atomSquareSVG from '../images/atom-square.svg';
+import PlaygroundComponent from "../components/playground";
 
 const IndexPage = () => {
   return (
       <Layout>
         <main className="container main-container">
           <section className="row section-block">
-            <div className="col text-center">
+            <div className="col-12 text-center">
               <h1 className="main-title mb-4"><StaticImage className="logo" src="../images/icon.png" alt="logo"/><span className="align-top">Nickel</span></h1>
               <div className="main-subtitle mb-4">Better configuration
                 for less
               </div>
 
               <div className="mt-4 mb-4 main-text">
-                Nickel is a configuration programming language. Write
-                complex configurations in a modular, correct and
+                Write complex configurations in a modular, correct and
                 boilerplate-free manner.
               </div>
             </div>
-            <div className="col">
-              <iframe title="asciinema" className="asciinema-iframe" allowFullScreen={true} webkitallowfullscreen="true" mozallowfullscreen="true"
-                      src="https://asciinema.org/a/3CG6L6oAfimqtDkoxiUu6VOws/iframe"/>
-            </div>
           </section>
+
+          <PlaygroundComponent/>
 
           <section className="row section-block">
             <div className="col-4 main-text text-center landingpage-column">
-              <img src={atomSVG} className="abstract-illustration" alt={""}/>
+              <img src={moleculeSVG} className="abstract-illustration" alt={""}/>
                 <h3 className="mb-4 mt-4">Merge</h3>
                 <div className="text-left">
-                    Write your configuration as logical blocks in a modular
-                      way.
-                      Combine them in a customizable way to assemble complex
-                      configurations.
+                    Write your configuration as simple, modular blocks.
+                   Merge them into a complex configuration.
                 </div>
             </div>
             <div className="col-4 main-text text-center landingpage-column">
               <img src={atomSquareSVG} className="abstract-illustration" alt={""}/>
                 <h3 className="mb-4 mt-4">Verify & Validate</h3>
               <div className="text-left">
-                Use types to specify and verify functions, if you want to. Let
-                the type inference engine do the boring work. Use contracts to
-                encode arbitrary schemas and validate your configuration data.
+                Use opt-in static typing to specify and verify functions if you need to. Let
+                type inference do the boring work. Use contracts
+                to validate your data and ensure they conform to a given schema.
               </div>
             </div>
             <div className="col-4 main-text text-center landingpage-column">
               <img src={moleculeSVG} className="abstract-illustration" alt={""}/>
                 <h3 className="mb-4 mt-4">Reuse</h3>
               <div className="text-left">
-                Don't hack, don't reinvent the wheel: Nickel is a proper
-                programming language. Factorize and reuse the generic aspects of
-                configuration, or use existing librairies.
+                Don't hack, don't reinvent the wheel: Nickel is a
+                programming language. Factorize and reuse the generic parts. Use existing libraries.
               </div>
             </div>
           </section>
