4c71f5a05acb48f428e4c6cdae12ab2403656d19
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-06-24 13:57:23 +0200

    Use custom colors in Bootstrap

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./src/styles/custom.scss";
 import "jquery/dist/jquery.min.js";
-import "popper.js/dist/popper.min";
+import "@popperjs/core/dist/umd/popper.min.js";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./src/styles/global.css";
