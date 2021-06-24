517b4147f259c6489cfde08484502e1fcc5ef1e8
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-06-24 17:45:01 +0200

    Title rewording

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ const IndexPage = () => {
 
           <section className={'row section-block'}>
             <div className="col-12 text-center">
-              <h1 className="mb-4">Try it out: find the error!</h1>
+              <h2 className="mb-4">Try it out. Find the error!</h2>
               <div className="mt-4 mb-4 main-text">
                 This configuration contains an error. Fix it and type <kbd>Ctrl</kbd>+<kbd>Enter</kbd> (or <kbd>Cmd <Command/>
               </kbd>+<kbd>Enter</kbd>) or click <span className={'btn btn-primary disabled'}>Run</span> to try your solution.
