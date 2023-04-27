8c459db3c9fe637bc55942c68b8652e2ef15c0dc
Viktor Kleen <viktor.kleen@tweag.io>
2023-04-27 12:45:03 +0000

    Add anchor links to stdlib function documentation

--- src/pages/stdlib/{StdlibSection.slug}.js
+++ src/pages/stdlib/{StdlibSection.slug}.js
@@ -10,6 +10,16 @@ import nickelLanguageDefinition from "../../prism/nickel";
 
 import ReactMarkdown from 'react-markdown';
 
+// Icon taken from gatsby-remark-autolink-headers at https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-remark-autolink-headers/src/index.js
+const AnchorIcon = () => {
+    return (
+       <svg aria-hidden="true" focusable="false" height="16" version="1.1" viewBox="0 0 16 16" width="16">
+        <path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z">
+        </path>
+       </svg>     
+    );
+}
+
 const Stdlib = ({data}) => {
     const object = JSON.parse(data.stdlibSection.internal.content);
     const {slug, name} = data.stdlibSection; 
@@ -19,18 +29,27 @@ const Stdlib = ({data}) => {
     };
 
     const HeaderWithTypes = ({id, name, types, contracts}) => {
+        const Header = ({content}) => {
+            return (
+                <h3 id={id} style={{'position': 'relative'}}>
+                    <a href={`#${id}`} aria-label={`${id} permalink`} class={`anchor before`}><AnchorIcon /></a>
+                    <code className={'language-nickel'}>{content}</code>
+                </h3>
+            );
+        }
+        
         if(!types && !contracts.length) {
-            return (<h3 id={id}><code className={'language-nickel'}>{name}</code></h3>);
+            return (<Header content={name} />);
         }
 
         if(types && !contracts.length) {
-            return (<h3 id={id}><code className={'language-nickel'}>{name} : {types}</code></h3>);
+            return (<Header content={`${name} : ${types}`} />);
         }
 
         if(!types && contracts) {
             return (
                 <React.Fragment>
-                <h3 id={id}><code className={'language-nickel'}>{name} | {contracts[0]}</code></h3>
+                <Header content={`${name} | ${contracts[0]}`} />
                 {contracts.slice(1).map((ctr) => {
                     return (<h4><code className={'language-nickel'}>{name} | {ctr}</code></h4>);
                 })}
@@ -40,7 +59,7 @@ const Stdlib = ({data}) => {
 
         return (
             <React.Fragment>
-            <h3 id={id}><code className={'language-nickel'}>{name} : {types}</code></h3>
+            <Header content={`${name} : ${types}`} />
             {contracts.map((ctr) => {
                 return (<h4><code className={'language-nickel'}>{name} | {ctr}</code></h4>);
             })}
